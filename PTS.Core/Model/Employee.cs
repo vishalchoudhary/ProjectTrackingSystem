df7c76299d10153bbe23fb3d3b8140651b7dc8fa
vikas kumar <vikaskumar4793@gmail.com>
2015-12-19 17:28:04 +0530

    Add Models Employee, UserStory

--- PTS.Core/Model/Employee.cs
+++ PTS.Core/Model/Employee.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace PTS.Core.Model
 {
-    class Employee
+    public class Employee
     {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Designation { get; set; }
+        public int ManagerID { get; set; }
+        public string ContactNo { get; set; }
+        public string EMailID { get; set; }
+        public string SkillSet { get; set; }
+
+        public virtual List<ProjectTask> ProjectTasks { get; set;}
     }
 }
--- PTS.Core/Model/UserStory.cs
+++ PTS.Core/Model/UserStory.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace PTS.Core.Model
 {
-    class UserStory
+    public class UserStory
     {
+        public int UserStoryID { get; set; }
+        public string Story { get; set; }
+        public int ProjectID { get; set; }
+
+        public virtual Project Projects { get; set; }
+        public virtual List<ProjectTask> ProjectTasks { get; set; }
+        
     }
 }
